245702be66a388a3a1f9bfd49aded8d1d5db85be
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 08:46:57 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+class Cat = Struct.new(:attr_names) do
+  def method_name
+    attr_accessor="maru"
+
+  end
+puts "meow"
+end
+maru= Cat.new
+mauu.name="Maru"
+maru.name
+maru.meow
